912eaa74de3849295374d01fff911e14e3639f89
Iker <flipas.garcia@gmail.com>
2018-02-04 12:01:48 +0100

    Rectangle fill and stroke

--- section1/script.js
+++ section1/script.js
@@ -1,3 +1,8 @@
 var canvas = document.getElementById("myCan");
 var ctx = canvas.getContext('2d');
-ctx.fillRect(100,100,440,280);
+ctx.fillRect(100,100,100,100); //x, y, w, h
+ctx.strokeRect(300, 300, 100, 100); //x, y, w, h
+ctx.fillStyle = 'yellow';
+ctx.rect(200, 200, 100, 100); //x, y, w, h
+ctx.stroke();
+ctx.fill();
