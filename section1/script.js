b638680c5f16710bca7b48bb7db543fe8ef78787
Iker <flipas.garcia@gmail.com>
2018-02-04 12:09:54 +0100

    clear rect

--- section1/script.js
+++ section1/script.js
@@ -1,8 +1,11 @@
 var canvas = document.getElementById("myCan");
 var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'white';
 ctx.fillRect(100,100,100,100); //x, y, w, h
 ctx.strokeRect(300, 300, 100, 100); //x, y, w, h
 ctx.fillStyle = 'yellow';
 ctx.rect(200, 200, 100, 100); //x, y, w, h
 ctx.stroke();
 ctx.fill();
+
+ctx.clearRect(150,150,100,100);
