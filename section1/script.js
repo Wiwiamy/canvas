2f796b045a44aab3e643b63d810955a7259426e0
Iker <flipas.garcia@gmail.com>
2018-02-04 13:19:15 +0100

    vurcas

--- section1/script.js
+++ section1/script.js
@@ -1,9 +1,12 @@
 var canvas = document.getElementById("myCan");
 var ctx = canvas.getContext('2d');
-//relleno
+
+/*
+
+ctx.save();
+ctx.beginPath();
 ctx.fillStyle = 'white';
 ctx.fillRect(100,100,100,100); //x, y, w, h
-ctx.save();
 //borde
 ctx.strokeStyle = 'white';
 ctx.strokeRect(300, 300, 100, 100); //x, y, w, h
@@ -19,20 +22,41 @@ ctx.restore();
 ctx.stroke();
 ctx.fill();
 
-ctx.restore();
-
-ctx.beginPath();
-//ctx.clearRect(0,0,640,480);
-
 ctx.shadowColor = 'black';
 ctx.shadowOffsetX = -20;
 ctx.shadowOffsetY = -20;
 ctx.shadowBlur = 10;
+
 ctx.strokeStyle = 'purple';
+
+ctx.beginPath();
 ctx.moveTo(200,200);
 ctx.lineTo(200, 400);
 ctx.lineTo(400, 400);
 ctx.lineTo(400, 200);
-ctx.lineTo(200, 200);
+ctx.closePath();
+//ctx.lineTo(200, 200);
+
+ctx.clearRect(0, 0, 640, 480);
+ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+
+ctx.stroke();
+//ctx.fill();
+*/
+ctx.fillStyle = 'white';
+ctx.strokeStyle = 'white';
+ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(50,350);
+ctx.quadraticCurveTo(50,200, 200, 100);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(50, 350);
+ctx.bezierCurveTo(50, 100, 300, 100,300, 350);
 ctx.stroke();
 ctx.closePath();
